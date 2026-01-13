da985072a74635dc9423db4800ef1ea9a3f5de56
Tulipani Gianpiero <gianpiero.tulipani@angel4future.com>
2026-01-13 14:26:11 +0100

    added gradient clipping

--- dragonnet/train.py
+++ dragonnet/train.py
@@ -152,6 +152,7 @@ def train_and_predict(
                 train_samples += x_batch.size(0)
 
                 loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
                 optimizer.step()
 
                 train_loss += loss.item()
